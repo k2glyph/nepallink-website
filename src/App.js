cd7a340c55a163b9faa802ce4eac765ab0257f2d
Dinesh Katwal <medineshkatwal@gmail.com>
2018-02-08 17:21:03 +0545

    change name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'antd/lib/button';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Button type="primary">Button</Button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome Nepalink Network Pvt.</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
